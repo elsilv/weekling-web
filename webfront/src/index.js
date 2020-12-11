f3b49e183be3fa6f4d6b34b7804f0c2484e0e6a8
elsilv <elsi.silvennoinen@gmail.com>
2020-12-11 16:07:00 +0200

    mahdollisuus valita kaikki ajat, sopivat ajat tai ei sopivat

--- webfront/src/index.js
+++ webfront/src/index.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import axios from 'axios'
 import './pohja.css';
 import logo from './weekling_logo_1.png';
 import { Notyf } from 'notyf';
 import 'notyf/notyf.min.css';
+import $ from "jquery"
 
 const App = () => {
+
+    const [status, setStatus] = useState(true)
 
     // Taulukko mahdollisista kellonajoista
     var timesArray = [
@@ -46,31 +49,36 @@ const App = () => {
         const params = new URLSearchParams(search)
         const id = params.get('id')
 
-        for (var i = 0; i < inputElements.length; ++i) {
-            if (inputElements[i].checked) {
-                var z = (inputElements[i].id).split(',')
-                var x = z[0]
-                var y = z[1].trimLeft()
+        function makeDate(boolean) {
+            for (var i = 0; i < inputElements.length; ++i) {
+                if (inputElements[i].checked == boolean) {
+                    var z = (inputElements[i].id).split(',')
+                    var x = z[0]
+                    var y = z[1].trimLeft()
 
-                // Luodaan tietojen pohjalta uusi Date()
-                var c = new Date()
-                c.setDate(daysArray[x].toString())
-                c.setHours(timesArray[y].toString())
-                c.setMinutes(0)
-                c.setSeconds(0)
-                c.setMonth(0)
-                c.setMilliseconds(0)
-                console.log(c)
+                    // Luodaan tietojen pohjalta uusi Date()
+                    var c = new Date()
+                    c.setDate(daysArray[x].toString())
+                    c.setHours(timesArray[y].toString())
+                    c.setMinutes(0)
+                    c.setSeconds(0)
+                    c.setMonth(0)
+                    c.setMilliseconds(0)
+                    console.log(c)
 
-                suitableTimes.push(c)
+                    suitableTimes.push(c)
+                }
             }
         }
+
+        if (status == false) { makeDate(false) }
+        else { makeDate(true)  }
 
         // Notifikaatiot lisäyksen onnistumisesta ja epäonnistumisesta
         const notyf = new Notyf()
         if (id == null || id.length != 24) { notyf.error('Id is wrong') }
-        if (suitableTimes.length === 0)    { notyf.error('Please, select some times before submitting') }
-        
+        if (suitableTimes.length === 0) { notyf.error('Please, select some times before submitting') }
+
         else {
             notyf.success('Success!')
             axios.put('/', {
@@ -89,6 +97,7 @@ const App = () => {
 
     window.onload = function () {
         createChecks(12)
+        addDayClick()
     }
 
     function checkColor(event) {
@@ -102,6 +111,20 @@ const App = () => {
         }
     }
 
+    function addDayClick() {
+        $('.dayName').each(function () {
+            var divId = $(this).parent().attr('id')
+            var parentDiv = $('#' + divId)
+
+            $(this).on("click", function () {
+                $('#' + divId).find(':checkbox').each(function () {
+                    $(this).prop('checked', true)
+                    $(this).parent().css('background-color', '#c3a41e')
+                })
+            })
+        })
+    }
+
     function checkColour(checkId, contId, x) {
         if (document.getElementById(checkId).checked) {
             console.log("klikattu")
@@ -112,12 +135,32 @@ const App = () => {
         }
     }
 
+    function checkBoxes(divId) {
+        $('#' + divId).find(':checkbox').each(function () {
+            $(this).prop('checked', true)
+        })
+    }
+
+    function selectAll() {
+        $('#kalenteri').find(':checkbox').each(function () {
+            $(this).prop('checked', true)
+            $(this).parent().css('background-color', '#c3a41e')
+        })
+    }
+
+    function unselectAll() {
+        $('#kalenteri').find(':checkbox').each(function () {
+            $(this).prop('checked', false)
+            $(this).parent().css('background-color', '#292929')
+        })
+    }
+
     function createChecks(maara) {
         for (let i = 1; i <= 7; i++) {
             var paiva = document.getElementById(i)
             var dayNow = new Date(Date.now()).getDay().toString()
-            if (dayNow == 0) { document.getElementById(7).style.backgroundColor = '#c3a41e'}
-            if (dayNow == i) { 
+            if (dayNow == 0) { document.getElementById(7).style.backgroundColor = '#c3a41e' }
+            if (dayNow == i) {
                 document.getElementById(i).style.backgroundColor = '#c3a41e'
             }
 
@@ -142,8 +185,6 @@ const App = () => {
     const search = window.location.search;
     const params = new URLSearchParams(search)
     const eventName = params.get('event')
-    var hours = params.get('hours') 
-    if(hours == undefined || null) hours = 12
 
     return (
         <html>
@@ -151,16 +192,35 @@ const App = () => {
                 <meta charset="UTF-8" />
                 <link type="text/css" href="pohja.css" />
                 <script src="index.js"> </script>
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
             </head>
             <body id="vartalo">
                 <div id="headerContainer">
-                    <a href="/"><img src={`${logo}`} alt="logo" /></a>
+                    <img src={`${logo}`} alt="logo" />
                     <div id="otsikko"> <h3>{`${eventName}`}</h3> </div>
                 </div>
                 <div id="header"></div>
                 <div id="instructions">
-                    <p id="instructions-text">Please select available times and press Send</p>
+                    <p id="instructions-text">Please select available times and press Send.
+                                              You can choose an entire day by clicking on the name, or change the input mode with 
+                                              the dropdown box above the calendar.</p>
                 </div>
+
+                <div id="buttons">
+                    <button onClick={() => setStatus(!status)}>{status ? 'Add available times' : 'Add non-available times'}</button>
+
+                    <div id="selectButtonDiv">
+                        <button id="selectButton" onClick={selectAll}>
+                            <div id="selectButtonText">Select all</div>
+                        </button>
+
+                        <button id="unselectButton" onClick={unselectAll}>
+                            <div id="unselectButtonText">Unselect all</div>
+                        </button>
+                    </div>
+                </div>
+
                 <div id="kalenteri">
                     <aside class="sivu">
                         <ul class="left">
@@ -208,29 +268,49 @@ const App = () => {
                     </aside>
                     <ul class="k">
                         <li class="date">
-                            <div class="group" id='1'>Monday</div>
+                            <div class="group" id='1'>
+                                <div class="dayName">Monday</div>
+                            </div>
                         </li>
                         <li class="date">
-                            <div class="group" id='2'>Tuesday</div>
+                            <div class="group" id='2' >
+                                <div class="dayName">Tuesday</div>
+                            </div>
                         </li>
                         <li class="date">
-                            <div class="group" id='3'>Wednesday</div>
+                            <div class="group" id='3' >
+                                <div class="dayName">Wednesday</div>
+                            </div>
                         </li>
                         <li class="date">
-                            <div class="group" id='4'>Thursday</div>
+                            <div class="group" id='4'>
+                                <div class="dayName">Thursday</div>
+                            </div>
                         </li>
                         <li class="date">
-                            <div class="group" id='5'>Friday</div>
+                            <div class="group" id='5'>
+                                <div class="dayName">Friday</div>
+                            </div>
                         </li>
                         <li class="date">
-                            <div class="group" id='6'>Saturday</div>
+                            <div class="group" id='6'>
+                                <div class="dayName">Saturday</div>
+                            </div>
                         </li>
                         <li class="date">
-                            <div class="group" id='7'>Sunday</div>
+                            <div class="group" id='7'>
+                                <div class="dayName">Sunday</div>
+                            </div>
                         </li>
                     </ul>
                 </div>
-                <button id="sendButton" onClick={handleSubmit}>Send</button>
+
+                <div id="buttondiv">
+                    <button id="sendButton" onClick={handleSubmit}>
+                        <div id="buttonText">Send</div>
+                    </button>
+                </div>
+
             </body>
         </html>
     )
